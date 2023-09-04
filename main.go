c730162c157618a4254d51d853e7128f713c4519
yaroslavvasilenko <yaroslav.vasilenko3101@gmail.com>
2023-09-04 14:20:28 +0300

    fix

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/yaroslavvasilenko/go_test_orm/poligon/ent"
 	sql_boiler_polygon "github.com/yaroslavvasilenko/go_test_orm/poligon/sql_boiler"
 	sql_boiler_storage "github.com/yaroslavvasilenko/go_test_orm/storage/sql_boiler"
 )
@@ -9,10 +10,12 @@ func main() {
 	// консольные команды необходимые для генерации кода
 	//   for ent go run -mod=mod entgo.io/ent/cmd/ent new User
 	//	go gerate ./orm/ent
-	//ent.TestEnt()
+	ent.TestEnt()
 
 	//  sqlboiler psql
 	sqlBoilerTest()
+
+	//	go run github.com/stephenafamo/bob/gen/bobgen-psql
 }
 
 func sqlBoilerTest() {
